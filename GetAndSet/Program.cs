07a1e09644fd78628667a03d2e06e51c74f5811a
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-18 16:54:38 +0100

    Added app that demonstrates the use of get and set

---
+++ GetAndSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAndSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Student student = new Student("Joe", "Rogan");
+
+                Console.WriteLine($"First student's name is: {student.GetFullName()}");
+                Console.WriteLine($"{student.Name} 's ID is: {student.Id}");
+
+                student.Name = "Jessie";
+
+                Console.WriteLine($"After changing their first name, the student with ID: {student.Id} is now called {student.GetFullName()}");
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ GetAndSet/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAndSet
+{
+    public class Student
+    {
+        /*
+         * Access methods for the _name property
+         */
+        public string Name { get; set; }
+
+        /*
+         * This property is read only as it only has a getter
+         */
+        public string LastName { get; }
+
+        /*
+         * This property can only be set internally (in this class)
+         */
+        public Guid Id { get; private set; }
+
+        public Student(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+            this.Id = Guid.NewGuid();   //create a global unique ID for each new student
+        }
+
+        public string GetFullName()
+        {
+            return Name + ' ' + LastName;
+        }
+    }
+}
