fc3d68e6282dfd68b856bb7c97c53dfddd786636
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-18 17:23:09 +0100

    Created app which demonstrates the use of extension methods

---
+++ StaticExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string word = "football";
+                Console.WriteLine($"String without extension method: {word}");
+                Console.WriteLine($"String with extension method: {word.FirstLetterUpperCase()}");
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ StaticExtensionMethods/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExtensionMethods
+{
+    /*
+     * Static class to act as a holder for static methods
+     * (should not and can not be instantiated)
+     */
+    public static class StringExtensions
+    {
+        /*
+         * Extension method for strings which converts the first letter
+         * of a string to upper case
+         */
+        public static string FirstLetterUpperCase(this string word)
+        {
+            char letter = Char.ToUpper(word[0]);
+            string remaining = word.Substring(1);
+
+            return letter + remaining;
+        }
+    }
+}
