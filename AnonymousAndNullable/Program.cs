cee2187dd28dc45f5d9f8eb0d24d626bdb9c9d2f
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-18 17:55:38 +0100

    Created app that demonstrates the use of anonnymous class and nullable types

---
+++ AnonymousAndNullable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousAndNullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Student student1 = new Student("Jenny", "Harper", 3);
+                Student student2 = new Student("Dorian", "Gray");
+
+                student1.PrintAwardInfo();
+                student2.PrintAwardInfo();
+
+                var anonymousStudent = new { Name = "John", LastName = "Belucci" }; // this is an object of the anonymous class
+                Console.WriteLine($"The student named {anonymousStudent.Name}  {anonymousStudent.LastName} doesn't appear in the register");
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ AnonymousAndNullable/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousAndNullable
+{
+    class Student
+    {
+        private uint? _awards = null;   //this is a nullable value
+        public string Name { get; }
+        public string LastName { get; }
+
+        public Student(string name, string lastName, uint? awards = null)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            _awards = awards; // the default value of awards if unasigned is null
+        }
+
+        /*
+         * Prints out how many awards the student has
+         */
+        public void PrintAwardInfo()
+        {
+            if(_awards.HasValue)
+            {
+                Console.WriteLine($"{GetFullName()} has {_awards} awards");
+            }
+            else
+            {
+                Console.WriteLine($"{GetFullName()} doesn't have awards");
+            }
+        }
+
+        private string GetFullName()
+        {
+            return Name + ' ' + LastName;
+        }
+    }
+}
