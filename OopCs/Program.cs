cf8c1dac5d36f16482099fbb266dfbfcc1438c8b
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-18 15:08:13 +0100

    Created Student class wrote default and overloaded constructor

--- OopCs/Program.cs
+++ OopCs/Program.cs
@@ -10,6 +10,18 @@ namespace OopCs
     {
         static void Main(string[] args)
         {
+            do
+            {
+                Student student = new Student(); //default constructor
+
+                Student student1 = new Student("John", "Doe"); //oveloaded constructor
+
+                Console.WriteLine($"First student name is {student.GetFullName()}");
+                Console.WriteLine($"Seccond student name is {student1.GetFullName()}");
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
---
+++ OopCs/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopCs
+{
+    /*
+     * Represents a data object of type Student
+     */
+    public class Student
+    {
+        /*
+         * Declare two strings as members of this classification
+         */
+        private string _name;
+        private string _lastName;
+
+        /*
+         * Default constructor for this class looks like this
+         */
+        public Student()
+        {
+            _name = string.Empty;
+            _lastName = string.Empty;
+        }
+
+        /*
+         * Overloads the default constructor to accept parameters
+         * these paremeters then get assigned to the class members declared above
+         * when the object is created.
+         */
+        public Student(string name, string lastName)
+        {
+            _name = name;
+            _lastName = lastName;
+        }
+
+        /*
+         * Returns a concatination of the two strings to display a full name
+         */
+        public string GetFullName()
+        {
+            return _name + ' ' + _lastName;
+        }
+    }
+}
