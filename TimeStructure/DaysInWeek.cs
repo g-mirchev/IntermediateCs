b2de4e89f0055cd55134835dbbef6a5d3d786160
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-20 12:52:09 +0100

    Added Enumeration to represent days of week

---
+++ TimeStructure/DaysInWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStructure
+{
+    /*
+     * An enumeration to represent the days of the week
+     */
+    public enum DaysInWeek : ushort
+    {
+        Monday = 1,     //make the first index 1 instead of 0
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
--- TimeStructure/Program.cs
+++ TimeStructure/Program.cs
@@ -8,7 +8,7 @@ namespace TimeStructure
 {
     /*
      * Takes hours, minutes and seconds from user input and prints out the time
-     * made from those values
+     * made from those values, then asks for day of the week and prints it out
      */
     class Program
     {
@@ -52,9 +52,55 @@ namespace TimeStructure
                 Time time = new Time(hours, minutes, seconds);
                 time.Printtime();
 
+                ushort day;
+
+                while(true)
+                {
+                    Console.WriteLine("Enter value for day of week from 1 to 7 (1 = Monday, 2 = Tuesday etc.)");
+                    if (ushort.TryParse(Console.ReadLine(), out day) && day > 0 && day <= 7) break;
+                    Console.WriteLine("Invalid entry, please try again..");
+                }
+
+                PrintDay(day); //prints the day of the week the user input corresponds to
+
                 Console.WriteLine();
                 Console.WriteLine("Press ESC to close or any key to run again.");
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        public static void PrintDay(ushort day)
+        {
+            switch (day)
+            {
+                case 1:
+                    DaysInWeek monday = DaysInWeek.Monday;
+                    Console.WriteLine(monday);
+                    break;
+                case 2:
+                    DaysInWeek tuesday = DaysInWeek.Tuesday;
+                    Console.WriteLine(tuesday);
+                    break;
+                case 3:
+                    DaysInWeek wednesday = DaysInWeek.Wednesday;
+                    Console.WriteLine(wednesday);
+                    break;
+                case 4:
+                    DaysInWeek thursday = DaysInWeek.Thursday;
+                    Console.WriteLine(thursday);
+                    break;
+                case 5:
+                    DaysInWeek friday = DaysInWeek.Friday;
+                    Console.WriteLine(friday);
+                    break;
+                case 6:
+                    DaysInWeek saturday = DaysInWeek.Saturday;
+                    Console.WriteLine(saturday);
+                    break;
+                case 7:
+                    DaysInWeek sunday = DaysInWeek.Sunday;
+                    Console.WriteLine(sunday);
+                    break;
+            }
         }
     }
 }
