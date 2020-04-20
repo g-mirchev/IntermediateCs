b5ccb597c96cefc9f0a717f9affad0e501621fc8
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-20 12:20:57 +0100

    Created app to demonstrate use of structures

---
+++ TimeStructure/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStructure
+{
+    /*
+     * Takes hours, minutes and seconds from user input and prints out the time
+     * made from those values
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                /*
+                 * Handles the input for hours, minutes and seconds
+                 */
+                uint hours;
+
+                while(true)
+                {
+                    Console.WriteLine("Enter value for hours (24h):");
+                    if (uint.TryParse(Console.ReadLine(), out hours) && hours <= 24) break;
+                    Console.WriteLine("Invalid entry, please try again..");
+                }
+
+                uint minutes;
+
+                while (true)
+                {
+                    Console.WriteLine("Enter value for minutes:");
+                    if (uint.TryParse(Console.ReadLine(), out minutes) && minutes < 60) break;
+                    Console.WriteLine("Invalid entry, please try again..");
+                }
+
+                uint seconds;
+
+                while (true)
+                {
+                    Console.WriteLine("Enter value for seconds:");
+                    if (uint.TryParse(Console.ReadLine(), out seconds) && seconds < 60) break;
+                    Console.WriteLine("Invalid entry, please try again..");
+                }
+
+                /*
+                 * Instantiate structure with user input and print the time
+                 */
+                Time time = new Time(hours, minutes, seconds);
+                time.Printtime();
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ TimeStructure/Time.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeStructure
+{
+    /*
+     * Creating structure to represent a time format in hours minutes and seconds
+     * the object created is immutable
+     */
+    public struct Time
+    {
+
+        private uint _hours, _minutes, _seconds;
+
+        /*
+         * The constructor must instantiate all values of the struct
+         */
+        public Time(uint hours, uint minutes, uint seconds)
+        {
+            _hours = hours;
+            _minutes = minutes;
+            _seconds = seconds;
+        }
+
+        /*
+         * Print out the time
+         */
+        public void Printtime()
+        {
+            Console.WriteLine($"The time is {_hours}:{_minutes}:{_seconds}");
+        }
+    }
+}
