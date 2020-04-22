5d72cb807d790920d52f7376baeb60bb19847330
g-mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-04-22 18:07:16 +0100

    Created an app that demonstrates the use of gneric types

---
+++ GenericTypes/CollectionInitializer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes
+{
+    /*
+     * A class to represent a collection of generic types
+     */
+    public class CollectionInitializer<T>
+    {
+        private T[] collection;     //Create an array of generic type
+        public uint CollectionLength { get; private set; }      //store the originally declared length
+
+        /*
+         * Declare the length of the collection and store it while creating an
+         * empty array of declared generic type
+         */
+        public CollectionInitializer(uint collectionLength)
+        {
+            collection = new T[collectionLength];
+            CollectionLength = collectionLength;
+        }
+
+        /*
+         * If items parsed are not more than the declared collection length
+         * add them to the collection. This will override any previous collection
+         */
+        public void AddElementsToCollection(params T[] elements)
+        {
+            if (elements.Length <= CollectionLength)
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    collection[i] = elements[i];
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Number of parameters can not exceed {CollectionLength}");
+            }
+        }
+
+        /*
+         * Returns the collection
+         */
+        public T[] RetrieveAllElements()
+        {
+            return collection;
+        }
+
+        /*
+         * Returns the item from this collection corresponding to the index
+         * if the index is within the bounds of the collection
+         */
+        public T RetrieveElementOnIndex(uint index)
+        {
+            if (index < CollectionLength)
+            {
+                return collection[index];
+            }
+            else
+            {
+                Console.WriteLine($"Index must be between 0 and {CollectionLength - 1}");
+                return default;
+            }
+        }
+    }
+}
---
+++ GenericTypes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                RunForInt();
+
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to close or any key to run again.");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static void RunForInt()
+        {
+            uint collectionLength;
+
+
+            /*
+             * Ask for user input for the collection lengt
+             */
+            while(true)
+            {
+                Console.WriteLine("How large should the collection be? (Don't go too big you will have to fill the collection yourself)");
+                if(uint.TryParse(Console.ReadLine(), out collectionLength)) break;
+                Console.WriteLine("Invalid input, please try again..");
+            }
+
+            CollectionInitializer<int> initializer = new CollectionInitializer<int>(collectionLength);
+
+            int[] inputCollection = new int[collectionLength];
+
+            /*
+             * Ask for user input for the array of elements
+             */
+            for (int i = 0; i < collectionLength; i++)
+            {
+                int input;
+                while(true)
+                {
+                    Console.WriteLine($"Write your element for position {i}");
+                    if (int.TryParse(Console.ReadLine(), out input)) break;
+                    Console.WriteLine("Invalid input, please try again..");
+                }
+                inputCollection[i] = input;
+            }
+            
+            initializer.AddElementsToCollection(inputCollection);
+            int[] collection = initializer.RetrieveAllElements();
+            PrintCollection<int>(collection);
+
+            uint index;
+
+            /*
+             * Ask for user input for the index ellement
+             */
+            while(true)
+            {
+                Console.WriteLine($"Select an element from your collection by index. The first is 0 the last is {collectionLength - 1}");
+                if (uint.TryParse(Console.ReadLine(), out index) && index < collectionLength) break;
+                Console.WriteLine("Invalid input, please try again..");
+            }
+
+            int number = initializer.RetrieveElementOnIndex(index);
+            Console.WriteLine();
+            Console.WriteLine($"Element on the index {index} is {number}");
+        }
+
+        /*
+         * Generic method to print array of any type
+         */
+        public static void PrintCollection<T>(params T[] elements)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Printing current collection:");
+            for(int i = 0; i < elements.Length; i ++)
+            {
+                Console.WriteLine(elements[i]);
+            }
+        }
+    }
+}
