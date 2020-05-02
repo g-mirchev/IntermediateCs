ce7f2f89cd5729125f3089a9db895ce10a2fc898
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-02 14:55:58 +0100

    Added student clas

---
+++ ListDictionary/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDictionary
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public ushort Year { get; set; }
+
+        public Student(string name, ushort year)
+        {
+            Name = name;
+            Year = year;
+        }
+    }
+}
