c34fba66625cb5cfb7e0cf4bfe0243cd4c35c2da
Georgi Mirchev <36302704+g-mirchev@users.noreply.github.com>
2020-05-07 17:26:50 +0100

    Added demonstration to main class

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
+    /*
+     * Demonstrates the use of function delegates
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            var executionManager = new ExecutionManager();
+            var operationManager = new OperationManager(20, 10, executionManager);
+
+            var sum = operationManager.Execute(Operation.Sum);
+            var substraction = operationManager.Execute(Operation.Substract);
+            var multiplication = operationManager.Execute(Operation.Multiply);
+
+            Console.WriteLine($"Result of sum: {sum} \nResult of substraction: {substraction} \nResult of multiplication: {multiplication}");
+
+            Console.WriteLine("\nPress any key to continue..");
+            Console.ReadKey();
+
         }
     }
 }
